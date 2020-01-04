892145f78231e99d2e9de97477c2cb4171b0ee2e
Justin Johnson <booleangate@gmail.com>
2020-01-03 23:49:43 -0800

    debugging

--- index.js
+++ index.js
@@ -21,12 +21,11 @@ function init() {
     const pullRequest = github.context.payload.pull_request;
 
     if (!pullRequest) {
-        core.info(github.context)
-        core.info('----------')
-        console.log(github.context.payload.pull_request)
-        console.log(github.context.payload)
+        core.info(github.context.payload)
         return [label, void 0];
     }
+
+    console.log(github.context.payload.pull_request)
 
     return [label, pullRequest.number];
 }
